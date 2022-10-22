44ea85856d7cc625dbb98eaf355c082e096f11dc
THE-BLUE-FRIEND <adrishg3@gmail.com>
2022-10-23 04:39:19 +0530

    Hey!

--- Secret.js
+++ Secret.js
@@ -4,55 +4,45 @@ function r(min,max)
 }
 function SecretMain()
 {
-    let needname=false;
-    window.addEventListener("keyup",function(event)
+    document.getElementById("setinfo").style.visibility="hidden";
+    let name=document.getElementById("needinfo").value;
+    let size;
+    for(size=r(5,7);name.length>size*6-5;size++);
+    let table=document.createElement("table");
+    document.getElementById("heart").appendChild(table);
+    for(let i=0;i<size*2-3+5*size/2;i++)
     {
-        if(!needname)
-        if(event.code=="Enter"||event.code=="NumpadEnter")
+        let tr=document.createElement("tr");
+        table.appendChild(tr);
+        for(let j=0;j<size*6-5;j++)
         {
-            needname=true;
-            document.getElementById("setinfo").style.visibility="hidden";
-            let name=document.getElementById("needinfo").value;
-            let size;
-            for(size=r(5,7);name.length>size*6-5;size++);
-            let table=document.createElement("table");
-            document.getElementById("heart").appendChild(table);
-            for(let i=0;i<size*2-3+5*size/2;i++)
-            {
-                let tr=document.createElement("tr");
-                table.appendChild(tr);
-                for(let j=0;j<size*6-5;j++)
-                {
-                    let td=document.createElement("td");
-                    td.id=`${i}td${j}`;
-                    tr.appendChild(td);
-                }
-            }
-            document.body.style.backgroundColor="rgb(0,0,0)";
-            for(let i=0;i<size;i++)
-            {
-                for(let j=size-i-1;j<size-i-1+size+2*i;j++)
-                document.getElementById(`${i}td${j}`).style.backgroundColor="rgb(255,0,0)";
-                for(let j=size*3-i+size-4;j<size*3-i+size-4+size+2*i;j++)
-                document.getElementById(`${i}td${j}`).style.backgroundColor="rgb(255,0,0)";
-            }
-            let love="I Love You";
-            for(let i=size;i<size*2-1;i++)
-            {
-                for(let j=0;j<(size*6-5);j++)
-                document.getElementById(`${i}td${j}`).style.backgroundColor="rgb(255,0,0)";
-                if(i==size+Math.floor(size/2)-1)
-                for(let j=Math.floor(((size*6-5)-love.length)/2);j<((size*6-5)+love.length)/2;j++)
-                document.getElementById(`${i}td${j}`).innerText=""+love.charAt(Math.floor(j-((size*6-5)-love.length)/2));
-                else if(i==size+Math.floor(size/2)+1)
-                for(let j=Math.floor(((size*6-5)-name.length)/2);j<((size*6-5)+name.length)/2;j++)
-                document.getElementById(`${i}td${j}`).innerText=""+name.charAt(Math.floor(j-((size*6-5)-name.length)/2));
-            }
-            for(let i=size*2-1;i<size*2-3+5*size/2;i++)
-            for(let j=i-(size*2-1)+1;j<(size*6-5)-(i-(size*2-1)+1);j++)
-            document.getElementById(`${i}td${j}`).style.backgroundColor="rgb(255,0,0)";
-            document.getElementById("share").style.visibility="visible";
+            let td=document.createElement("td");
+            td.id=`${i}td${j}`;
+            tr.appendChild(td);
         }
     }
-    );
+    document.body.style.backgroundColor="rgb(0,0,0)";
+    for(let i=0;i<size;i++)
+    {
+        for(let j=size-i-1;j<size-i-1+size+2*i;j++)
+        document.getElementById(`${i}td${j}`).style.backgroundColor="rgb(255,0,0)";
+        for(let j=size*3-i+size-4;j<size*3-i+size-4+size+2*i;j++)
+        document.getElementById(`${i}td${j}`).style.backgroundColor="rgb(255,0,0)";
+    }
+    let love="I Love You";
+    for(let i=size;i<size*2-1;i++)
+    {
+        for(let j=0;j<(size*6-5);j++)
+        document.getElementById(`${i}td${j}`).style.backgroundColor="rgb(255,0,0)";
+        if(i==size+Math.floor(size/2)-1)
+        for(let j=Math.floor(((size*6-5)-love.length)/2);j<((size*6-5)+love.length)/2;j++)
+        document.getElementById(`${i}td${j}`).innerText=""+love.charAt(Math.floor(j-((size*6-5)-love.length)/2));
+        else if(i==size+Math.floor(size/2)+1)
+        for(let j=Math.floor(((size*6-5)-name.length)/2);j<((size*6-5)+name.length)/2;j++)
+        document.getElementById(`${i}td${j}`).innerText=""+name.charAt(Math.floor(j-((size*6-5)-name.length)/2));
+    }
+    for(let i=size*2-1;i<size*2-3+5*size/2;i++)
+    for(let j=i-(size*2-1)+1;j<(size*6-5)-(i-(size*2-1)+1);j++)
+    document.getElementById(`${i}td${j}`).style.backgroundColor="rgb(255,0,0)";
+    document.getElementById("share").style.visibility="visible";
 }
